97180f6279e68b1e7897e31a19495b76db28c39b
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-03-31 19:43:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,29 +7,30 @@
 int main(){
     list<int> lst;
 
+    // ====== APPEND ======
     auto allStart = std::chrono::high_resolution_clock::now();
-
     auto appendStart = std::chrono::high_resolution_clock::now();
+    std::cout << "Appending " << BENCHMARK_N << " elements..." << std::endl;
     for(int i=0; i<BENCHMARK_N; i++){
         lst.append(i);
     }
     auto appendEnd = std::chrono::high_resolution_clock::now();
-
+    // ====== GET ======
     auto getStart = std::chrono::high_resolution_clock::now();
+    std::cout << "Getting " << BENCHMARK_N << " elements and placing them into an array..." << std::endl;
     int* arr = new int[BENCHMARK_N];
     for(int i=0; i<BENCHMARK_N; i++){
         arr[i] = lst.get(i);
     }
     delete arr;
     auto getEnd = std::chrono::high_resolution_clock::now();
-
+    // ====== REMOVE ======
     auto removeStart = std::chrono::high_resolution_clock::now();
-    for(int i=0; i<BENCHMARK_N; i++){
-        lst.remove(0);
+    std::cout << "Removing " << BENCHMARK_N << " elements (with worst case scenario (removing last element))..." << std::endl;
+    while(lst.len()){
+        lst.remove(lst.len()-1);
     }
     auto removeEnd = std::chrono::high_resolution_clock::now();
-
-
     auto allEnd = std::chrono::high_resolution_clock::now();
 
     std::cout << "Benchmark results with a list of " << BENCHMARK_N << " elements:" << std::endl;
