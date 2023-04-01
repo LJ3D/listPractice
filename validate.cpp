00d38dab03662c1d76bafe25751fafed1e02231a
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-04-01 11:06:21 +0100

    make work good yes

--- validate.cpp
+++ validate.cpp
@@ -3,85 +3,110 @@
 #include <list> // For comparison
 #include "list.hpp"
 
+bool validateLists(LJLIST::list<int> l_list, std::list<int> s_list){
+    if(l_list.len() != s_list.size()){
+        std::cout << "ERROR: " << l_list.len() << " != " << s_list.size() << std::endl;
+        return false;
+    }
+    std::list<int>::iterator s_it = s_list.begin();
+    for(int i=0; i<l_list.len(); i++){
+        if(l_list.get(i) != *s_it){
+            std::cout << "ERROR: " << l_list.get(i) << " != " << *s_it << std::endl;
+            return false;
+        }
+        s_it++;
+    }
+    return true;
+}
+
+template<typename T>
+bool arrCmp(T* arr1, T* arr2, int len){
+    for(int i=0; i<len; i++){
+        if(arr1[i] != arr2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+#define VALIDATE_N 1000
+
+
 int main(){
-    LJLIST::list<int> l_list;
-    std::list<int> s_list;
+    LJLIST::list<int> lst;
+    std::list<int> s_lst;
 
-    // Append some numbers
-    std::cout << "Appending numbers..." << std::endl;
-    for(int i=0; i<1000; i++){
-        l_list.append(i);
-        s_list.push_back(i);
+    for(int i=0; i<VALIDATE_N; i++){
+        lst.append(i);
+        s_lst.push_back(i);
     }
 
-    // Get numbers at random positions
-    std::cout << "Getting numbers at random positions..." << std::endl;
-    std::list<int>::iterator s_it;
-    for(int i=0; i<1000; i++){
-        int idx = rand()%l_list.len();
-        int l_val = l_list.get(idx);
-        s_it = s_list.begin();
-        for(int j=0; j<idx; j++){
-            s_it++;
-        }
-        int s_val = *s_it;
-        if(l_val != s_val){
-            std::cout << "ERROR: " << l_val << " != " << s_val << std::endl;
-        }
+    // Visit random elements of each list and place them into an array
+    std::list<int>::iterator s_it = s_lst.begin();
+    int* arr = new int[VALIDATE_N];
+    int* arr2 = new int[VALIDATE_N];
+    for(int i=0; i<VALIDATE_N; i++){
+        int idx = rand()%lst.len();
+        arr[i] = lst.get(idx);
+        s_it = s_lst.begin();
+        std::advance(s_it, idx);
+        arr2[i] = *s_it;
+    }
+    bool valid = arrCmp(arr, arr2, VALIDATE_N);
+    delete arr2;
+    delete arr;
+    if(!valid){
+        std::cout << "ERROR: Random element validation failed!" << std::endl;
+        return 1;
+    }
+    std::cout << "Random element validation successful!" << std::endl;
+
+    // Visit sequential elements of each list and place them into an array
+    s_it = s_lst.begin();
+    arr = new int[VALIDATE_N];
+    arr2 = new int[VALIDATE_N];
+    for(int i=0; i<VALIDATE_N; i++){
+        arr[i] = lst.get(i);
+        arr2[i] = *s_it;
+        s_it++;
+    }
+    valid = arrCmp(arr, arr2, VALIDATE_N);
+    delete arr2;
+    delete arr;
+    if(!valid){
+        std::cout << "ERROR: Sequential element validation failed!" << std::endl;
+        return 1;
+    }
+    std::cout << "Sequential element validation successful!" << std::endl;
+
+    // Remove half of the elements from each list randomly
+    for(int i=0; i<VALIDATE_N/2; i++){
+        int idx = rand()%lst.len();
+        lst.remove(idx);
+        s_it = s_lst.begin();
+        std::advance(s_it, idx);
+        s_lst.erase(s_it);
     }
 
-    // Remove a few numbers at random positions
-    std::cout << "Removing numbers at random positions..." << std::endl;
-    for(int i=0; i<100; i++){
-        int idx = rand()%l_list.len();
-        l_list.remove(idx);
-        s_it = s_list.begin();
-        for(int j=0; j<idx; j++){
-            s_it++;
-        }
-        s_list.erase(s_it);
+    // Now get random elements from each list and place them into an array again
+    s_it = s_lst.begin();
+    arr = new int[VALIDATE_N];
+    arr2 = new int[VALIDATE_N];
+    for(int i=0; i<VALIDATE_N; i++){
+        int idx = rand()%lst.len();
+        arr[i] = lst.get(idx);
+        s_it = s_lst.begin();
+        std::advance(s_it, idx);
+        arr2[i] = *s_it;
     }
-    // Get numbers at random positions (again)
-    std::cout << "Getting numbers at random positions..." << std::endl;
-    for(int i=0; i<1000; i++){
-        int idx = rand()%l_list.len();
-        int l_val = l_list.get(idx);
-        s_it = s_list.begin();
-        for(int j=0; j<idx; j++){
-            s_it++;
-        }
-        int s_val = *s_it;
-        if(l_val != s_val){
-            std::cout << "ERROR: " << l_val << " != " << s_val << std::endl;
-        }
+    valid = arrCmp(arr, arr2, VALIDATE_N);
+    delete arr2;
+    delete arr;
+    if(!valid){
+        std::cout << "ERROR: Random element validation failed after removal!" << std::endl;
+        return 1;
     }
-
-    // Insert a few numbers at random positions
-    std::cout << "Inserting numbers at random positions..." << std::endl;
-    for(int i=0; i<100; i++){
-        int idx = rand()%l_list.len();
-        l_list.insert(i, idx);
-        s_it = s_list.begin();
-        for(int j=0; j<idx; j++){
-            s_it++;
-        }
-        s_list.insert(s_it, i);
-    }
-
-    // Get numbers at random positions (again)
-    std::cout << "Getting numbers at random positions..." << std::endl;
-    for(int i=0; i<1000; i++){
-        int idx = rand()%l_list.len();
-        int l_val = l_list.get(idx);
-        s_it = s_list.begin();
-        for(int j=0; j<idx; j++){
-            s_it++;
-        }
-        int s_val = *s_it;
-        if(l_val != s_val){
-            std::cout << "ERROR: " << l_val << " != " << s_val << std::endl;
-        }
-    }
+    std::cout << "Random element validation successful after removal!" << std::endl;
 
     return 0;
 }
